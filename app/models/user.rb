a91751ecd8fc09c025607aa4ee63dbe206fbf3b0
marin72_com <sifon1994@gmail.com>
2015-08-19 22:11:37 +0900

    support logging in using twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_auth_hash(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    image_url = auth_hash[:info][:image]
+    name = auth_hash[:info][:name]
+    p provider: provider, uid: uid, image_url: image_url, name: name
+
+    User.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.name = name
+      user.image_url = image_url
+    end
+  end
 end
