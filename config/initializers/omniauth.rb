ddd086fa96cf9559cb9b0f636fda9a88f22e0ff3
marin72_com <sifon1994@gmail.com>
2015-08-26 19:18:54 +0900

    return to the original

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,13 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV["TWITTER_CONSUMER_KEY"],
     ENV["TWITTER_CONSUMER_SECRET"]
 
-  if Rails.env.development?
-    provider :facebook,
-      ENV["FACEBOOK_CONSUMER_KEY"],
-      ENV["FACEBOOK_CONSUMER_SECRET"]
-  else 
-    provider :facebook,
-      ENV["FACEBOOK_CONSUMER_KEY"],
-      ENV["FACEBOOK_CONSUMER_SECRET"]
-  end
+  provider :facebook,
+    ENV["FACEBOOK_CONSUMER_KEY"],
+    ENV["FACEBOOK_CONSUMER_SECRET"]
 end
