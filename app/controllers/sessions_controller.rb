01cce5aa43ff2ed51445f1f8db9e9a0a4d4547f3
marin72_com <sifon1994@gmail.com>
2015-08-26 15:42:37 +0900

    change redirect page when logged in, redirect push_page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to session.delete(:back_url) || root_path, notice: 'successfully logged in'
+    redirect_to new_push_path, notice: 'click image'
+    #redirect_to root_path || session.delete(:back_url), notice: 'successfully logged in'
   end
 
   def destroy
